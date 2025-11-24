c89be8419c4cdddead83c66748a3d11eb7ab1aba
Hayden <hayden@funwae.com>
2025-11-23 21:58:55 -0600

    Fix missing imports in statement detail page

--- packages/console/src/app/app/marketplace/payouts/[month]/page.tsx
+++ packages/console/src/app/app/marketplace/payouts/[month]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import Layout from '@/components/Layout';
 
 export default function StatementDetailPage() {
